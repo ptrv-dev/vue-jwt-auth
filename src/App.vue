d182d21b7743a7cb5aec3b2515fe88a471062212
ptrv-dev <petrpv99119@gmail.com>
2023-05-09 19:52:43 +0300

    App component updates (add header and logout functionality)

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,38 @@
 <template>
+  <header class="header">
+    <div v-if="!userData" class="container">
+      <RouterLink to="/login">Sign In</RouterLink>
+      <RouterLink to="/registration">Sign Up</RouterLink>
+    </div>
+    <div v-else class="container">
+      <button @click="handleLogout">Logout</button>
+    </div>
+  </header>
   <RouterView></RouterView>
 </template>
+
+<script>
+import authService from './services/authService';
+
+export default {
+  mounted() {
+    this.fetchMe();
+  },
+  data() {
+    return {
+      userData: null,
+    };
+  },
+  methods: {
+    async fetchMe() {
+      this.userData = await authService.getMe();
+    },
+    async handleLogout() {
+      await authService.logout();
+      this.userData = null;
+      window.localStorage.removeItem('token');
+      alert('Logout successfully!');
+    },
+  },
+};
+</script>
